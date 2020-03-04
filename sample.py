18e54ddfa4a37942689c014b07f51887adb2a366
Eggert Már Eggertsson <eggerte19@ru.is>
2020-03-04 12:31:06 +0000

    added a couple of edge cases to be safe

--- sample.py
+++ sample.py
@@ -1,5 +1,7 @@
 def fizzbuzz(num):
-    if num % 5 == 0 and num %3 == 0:
+    if num == 0:
+        return num
+    elif num % 5 == 0 and num %3 == 0:
         return "FizzBuzz"
     elif num % 5 == 0:
         return "Buzz"
--- test_sample.py
+++ test_sample.py
@@ -2,9 +2,20 @@ from sample import fizzbuzz
 
 def test_divisible_by_both():
     assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(30) == "FizzBuzz"
+    assert fizzbuzz(-15) == "FizzBuzz"
 def test_divisible_by_five():
+    assert fizzbuzz(-5) == "Buzz"
     assert fizzbuzz(5) == "Buzz"
 def test_divisible_by_three():
     assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(-33) == "Fizz"
+    assert fizzbuzz(-3) == "Fizz"
+    assert fizzbuzz(-6) == "Fizz"
 def test_if_not_divisible_by_any():
     assert fizzbuzz(8) == 8
+    assert fizzbuzz(-8) == -8
+    assert fizzbuzz(-2) == -2
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(-1) == -1
+
