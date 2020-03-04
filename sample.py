5315b8532ce1b08c49ae978d6964d8e609764f44
Eggert Már Eggertsson <eggerte19@ru.is>
2020-03-04 12:03:05 +0000

    all completed

--- sample.py
+++ sample.py
@@ -1,7 +1,9 @@
 def fizzbuzz(num):
     if num % 5 == 0 and num %3 == 0:
         return "FizzBuzz"
-    if num % 5 == 0:
+    elif num % 5 == 0:
         return "Buzz"
-    if num % 3 == 0:
+    elif num % 3 == 0:
         return "Fizz"
+    else:
+        return num
