e747a7f31e0f1b4e5d5e49a61fc3f37008fee700
Eggert Már Eggertsson <eggerte19@ru.is>
2020-03-04 12:21:34 +0000

    did not realize that it should have been four different test functions, fixed that

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,10 @@
 from sample import fizzbuzz
 
-def test_return_fizzbuzz():
+def test_divisible_by_both():
     assert fizzbuzz(15) == "FizzBuzz"
+def test_divisible_by_five():
     assert fizzbuzz(5) == "Buzz"
+def test_divisible_by_three():
     assert fizzbuzz(3) == "Fizz"
+def test_if_not_divisible_by_any():
     assert fizzbuzz(8) == 8
