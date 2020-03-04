072b2be990903c7f06b61a706fdf8be618641df9
Eggert Már Eggertsson <eggerte19@ru.is>
2020-03-04 11:29:48 +0000

    second test completed

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
 def fizzbuzz(num):
     if num % 5 == 0 and num %3 == 0:
         return "FizzBuzz"
+
+    
--- test_sample.py
+++ test_sample.py
@@ -2,3 +2,4 @@ from sample import fizzbuzz
 
 def test_return_fizzbuzz():
     assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(5) == "FizzBuzz"
